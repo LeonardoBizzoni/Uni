30d2a9a6b9199f83e632f2e9982f2f3d2a751139
LeonardoBizzoni <leo2002714@gmail.com>
2022-10-29 14:57:07 +0200

    Funzione per aggiungere Node

--- PrimoAnno/fondamentiInformatica/28-10-22/src/binary_tree.rs
+++ PrimoAnno/fondamentiInformatica/28-10-22/src/binary_tree.rs
@@ -3,68 +3,99 @@ pub struct Node {
     pub left: Option<Box<Node>>,
     pub right: Option<Box<Node>>,
 
-    pub visited: bool,
+    pub pos: usize,
 }
 
 pub struct BinaryTree {
-    pub root: Node,
+    pub root: Box<Node>,
 }
 
 impl BinaryTree {
-    pub fn new(value: u8) -> Self {
-        Self {
-            root: Node {
-                value,
-                left: None,
-                right: None,
-                visited: true,
-            },
-        }
+    pub fn new(value: u8, pos: usize) -> Self {
+        Self { root: Box::new(Node { value, left: None, right: None, pos }) }
     }
 
-    pub fn add_node(&mut self, value: u8, current_node: Option<Box<Node>>) {
-        // Spaghetti code incoming
-        if let Some(mut current_node) = current_node {
-            // current_node must be a generic non-root Node
+    pub fn print(self) -> String {
+	let mut res = String::new();
+	let mut test: Vec<u8> = Vec::new();
+	BinaryTree::traverse_tree(&mut res, &String::from(""), &String::from(""), &Some(self.root), &mut test);
 
-            if current_node.left.is_none() {
-                current_node.left = Some(Box::new(Node {
-                    value,
-                    left: None,
-                    right: None,
-                    visited: false,
-                }));
-            } else if current_node.right.is_none() {
-                current_node.right = Some(Box::new(Node {
-                    value,
-                    left: None,
-                    right: None,
-                    visited: false,
-                }));
-            } else {
-                self.add_node(value, Some(current_node.left.unwrap()));
-            }
-        } else {
-            // current_node must be root
+	println!("{:?}", test);
 
-            if self.root.left.is_none() {
-                self.root.left = Some(Box::new(Node {
-                    value,
-                    left: None,
-                    right: None,
-                    visited: false,
-                }));
-            } else if self.root.right.is_none() {
-                self.root.right = Some(Box::new(Node {
-                    value,
-                    left: None,
-                    right: None,
-                    visited: false,
-                }));
-            } else {
-                // if root-left !visited call add_node(root-left)
-                // else call add_node(root-right)
-            }
+	res
+    }
+
+    fn traverse_tree(str: &mut String, padding: &String, pointer: &String, node: &Option<Box<Node>>, test: &mut Vec<u8>) {
+	if let Some(node) = node {
+	    str.push_str(&padding);
+	    str.push_str(&pointer);
+	    str.push_str("Node value: ");
+	    str.push_str(&node.value.to_string());
+	    str.push('\n');
+
+	    test.push(node.value);
+
+	    let mut padding_builder = String::from(padding);
+	    padding_builder.push_str("│  ");
+
+	    let padding_both = String::from(padding_builder);
+	    let pointer_right = String::from("└──");
+	    let pointer_left = match node.right {
+		Some(_) => String::from("├──"),
+		None => String::from("└──")
+	    };
+	    
+	    BinaryTree::traverse_tree(str, &padding_both, &pointer_left, &node.left, test);
+	    BinaryTree::traverse_tree(str, &padding_both, &pointer_right, &node.right, test);
+	}
+    }
+
+    fn recursive_add_node(current: &mut Option<Box<Node>>, value: u8, pos: usize, prev_pos: &usize) -> Result<(), ()>{
+	match current {
+	    Some(branch) => {
+		if (pos as f64)/2.0 == branch.pos as f64 {
+		    // add left
+		    let _ = BinaryTree::recursive_add_node(&mut branch.left, value, pos, &branch.pos);
+		    return Ok(());
+		} else if ((pos-1) as f64)/2.0 == branch.pos as f64 {
+		    // add right
+		    let _ = BinaryTree::recursive_add_node(&mut branch.right, value, pos, &branch.pos);
+		    return Ok(());
+		} else {
+		    if let Err(_) = BinaryTree::recursive_add_node(&mut branch.left, value, pos, &branch.pos) {
+			match BinaryTree::recursive_add_node(&mut branch.right, value, pos, &branch.pos) {
+			    Ok(_) => return Ok(()),
+			    Err(_) => return Err(())
+			}
+		    }
+
+		    return Ok(());
+		}
+	    }
+	    None => {
+		if prev_pos*2 == pos || prev_pos * 2 +1 == pos {
+		    *current = Some(Box::new(Node {value, left: None, right: None, pos}));
+		    return Ok(());
+		}
+
+		Err(())
+	    }
+	}
+    }
+
+    pub fn add_node(&mut self, value: u8, pos: usize) {
+        if value != 0 {
+	    if (pos as f64)/2.0 == self.root.pos as f64 {
+		// add left
+		self.root.left = Some(Box::new(Node { value, left: None, right: None, pos }));
+	    } else if ((pos-1) as f64)/2.0 == self.root.pos as f64 {
+		// add right
+		self.root.right = Some(Box::new(Node { value, left: None, right: None, pos }));
+	    } else {
+		if let Err(_) = BinaryTree::recursive_add_node(&mut self.root.left, value, pos, &1) {
+		    let _ = BinaryTree::recursive_add_node(&mut self.root.right, value, pos, &1);
+		}
+	    }
         }
     }
 }
--- PrimoAnno/fondamentiInformatica/28-10-22/src/main.rs
+++ PrimoAnno/fondamentiInformatica/28-10-22/src/main.rs
@@ -1,36 +1,61 @@
 mod binary_tree;
 mod utils;
 
-use std::io;
+use std::{io, process::ExitCode};
 
-fn main() {
+fn main() -> ExitCode {
     let mut binary_str = String::new();
     let mut tree_value_input = String::new();
+    // let mut num: u8;
 
-    print!("Enter binary tree structure: ");
+    println!("Enter binary tree structure: ");
     if let Err(msg) = io::stdin().read_line(&mut binary_str) {
         eprintln!("{}", msg);
+        return ExitCode::FAILURE;
     }
+    let mut binary_str = binary_str.trim().to_string();
 
     if let Err(msg) = utils::check_syntax(&binary_str) {
         eprintln!("{}", msg);
+        return ExitCode::FAILURE;
     }
 
-    let mut tree = binary_tree::BinaryTree::new(1);
-    let mut num: u8;
+    binary_str.remove(0);
+    println!("Enter root node value: ");
+    io::stdin()
+        .read_line(&mut tree_value_input)
+        .expect("Couldn't read value from stdin");
+    let mut tree_root = binary_tree::BinaryTree::new(tree_value_input.trim().parse().expect("NaN"), 1);
 
-    for ch in binary_str.chars() {
-	num = 0;
-	
-        if ch == '1' {
-            print!("Enter node value: ");
-            io::stdin()
-                .read_line(&mut tree_value_input)
-                .expect("Couldn't read value from stdin");
+    tree_root.add_node(2, 2);
+    tree_root.add_node(30, 3);
 
-            num = tree_value_input.trim().parse().expect("NaN");
-        }
+    tree_root.add_node(4, 4);
+    tree_root.add_node(6, 6);
 
-	tree.add_node(num, None);
-    }
+    tree_root.add_node(8, 8);
+    tree_root.add_node(13, 13);
+
+    tree_root.add_node(15, 15);
+    tree_root.add_node(17, 17);
+
+    // for (pos, ch) in binary_str.chars().enumerate() {
+    //     let mut tree_value_input = String::new();
+    //     num = 0;
+
+    //     if ch == '1' {
+    //         println!("Enter node value: ");
+    //         io::stdin()
+    //             .read_line(&mut tree_value_input)
+    //             .expect("Couldn't read value from stdin");
+
+    //         num = tree_value_input.trim().parse().expect("NaN");
+    //     }
+
+    //     tree_root.add_node(num, pos+2);
+    // }
+
+    println!("{}", tree_root.print());
+
+    ExitCode::SUCCESS
 }
