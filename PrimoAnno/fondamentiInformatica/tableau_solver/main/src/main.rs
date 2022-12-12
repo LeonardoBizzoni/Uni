7c93b29935a8c3c747f51e9d2eb38ba3877b2c62
LeonardoBizzoni <leo2002714@gmail.com>
2022-12-12 20:37:57 +0100

    controllo correttezza di una formula ben formata

---
+++ PrimoAnno/fondamentiInformatica/tableau_solver/main/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+use tableau::Formula;
+
+fn main() -> io::Result<()> {
+    let mut input_buffer = String::new();
+
+    println!("Enter formula:");
+    if let Err(err) = io::stdin().read_line(&mut input_buffer) {
+	eprintln!("[ERROR] failed to read from stdin.");
+	return Err(err);
+    }
+
+    let input_buffer: String = input_buffer.trim().to_string();
+
+    if Formula::check_syntax(&input_buffer) {
+	println!("epico");
+    } else {
+	println!("Invalid formula");
+    }
+
+    Ok(())
+}
