2d985fac7968d41808f6c544551b42ba0646399c
LeonardoBizzoni <leo2002714@gmail.com>
2022-12-14 12:29:12 +0100

    Fixato operatore not

--- PrimoAnno/fondamentiInformatica/tableau_solver/tableau/src/tree.rs
+++ PrimoAnno/fondamentiInformatica/tableau_solver/tableau/src/tree.rs
@@ -14,20 +14,28 @@ impl Tree {
 
         match Self::get_root(&op_stack) {
             Some((root, idx)) => {
-                let mut left  = &formula[..idx];
-                let mut right = &formula[idx+1..];
+                let mut left = &formula[..idx];
+                let mut right = &formula[idx + 1..];
 
-		if left.chars().nth(0) == Some('(') {
-		    left = &formula[1..idx];
-		}
-		if right.chars().nth(0) == Some('(') {
-		    right = &formula[idx+1..formula.len()-1];
+                if left.chars().nth(0) == Some('(') {
+                    left = &formula[1..idx];
+                }
+                if right.chars().nth(0) == Some('(') {
+                    right = &formula[idx + 2..formula.len() - 1];
+                }
+
+		if idx != 0 {
+                    return Self {
+			root,
+			left: Some(Box::from(Tree::new(left))),
+			right: Some(Box::from(Tree::new(right))),
+                    };
 		}
 
                 Self {
-                    root,
-                    left: Some(Box::from(Tree::new(left))),
-                    right: Some(Box::from(Tree::new(right))),
+		    root,
+		    left: None,
+		    right: Some(Box::from(Tree::new(right))),
                 }
             }
             None => Self {
@@ -43,54 +51,55 @@ impl Tree {
         // Se il primo elemento è un atomo allora il prossimo elemento deve essere un operatore binario ed l'operatore principale
         // Se il primo elemento è una parentesi richiama il metodo con il contenuto della parentesi
 
-        if matches!(op_stack[0], ElementType::Unary) {
-            return Some(('!', 0));
-        } else if matches!(op_stack[0], ElementType::Atom(_)) {
-            if op_stack.len() >= 2 {
-                match op_stack[1] {
-                    ElementType::Binary('&') => return Some(('&', 1)),
-                    ElementType::Binary('|') => return Some(('|', 1)),
-                    ElementType::Binary('>') => return Some(('>', 1)),
-                    ElementType::Binary('-') => return Some(('-', 1)),
-                    _ => {}
-                }
-            }
-
-            return None;
-        } else if matches!(op_stack[0], ElementType::NestOpen) {
-            let mut nest_stack: Vec<usize> = Vec::new();
-            let mut root_idx: isize = -1;
-
-            for (idx, op) in op_stack.iter().enumerate() {
-                match op {
-                    ElementType::NestOpen => nest_stack.push(1),
-                    ElementType::NestClose => {
-                        nest_stack.pop();
-
-                        if nest_stack.len() == 0 {
-                            root_idx = idx as isize;
-                            break;
-                        }
+        if op_stack.len() > 0 {
+            if matches!(op_stack[0], ElementType::Unary) {
+                return Some(('!', 0));
+            } else if matches!(op_stack[0], ElementType::Atom(_)) {
+                if op_stack.len() >= 2 {
+                    match op_stack[1] {
+                        ElementType::Binary('&') => return Some(('&', 1)),
+                        ElementType::Binary('|') => return Some(('|', 1)),
+                        ElementType::Binary('>') => return Some(('>', 1)),
+                        ElementType::Binary('-') => return Some(('-', 1)),
+                        _ => {}
                     }
-                    _ => {}
                 }
-            }
 
-            if root_idx != -1 {
-                let root_idx: usize = root_idx as usize + 1;
+                return None;
+            } else if matches!(op_stack[0], ElementType::NestOpen) {
+                let mut nest_stack: Vec<usize> = Vec::new();
+                let mut root_idx: isize = -1;
 
-                match op_stack[root_idx] {
-                    ElementType::Binary('&') => return Some(('&', root_idx)),
-                    ElementType::Binary('|') => return Some(('|', root_idx)),
-                    ElementType::Binary('>') => return Some(('>', root_idx)),
-                    ElementType::Binary('-') => return Some(('-', root_idx)),
-                    _ => {}
+                for (idx, op) in op_stack.iter().enumerate() {
+                    match op {
+                        ElementType::NestOpen => nest_stack.push(1),
+                        ElementType::NestClose => {
+                            nest_stack.pop();
+
+                            if nest_stack.len() == 0 {
+                                root_idx = idx as isize;
+                                break;
+                            }
+                        }
+                        _ => {}
+                    }
                 }
-            }
 
-            return None;
+                if root_idx != -1 {
+                    let root_idx: usize = root_idx as usize + 1;
+
+                    match op_stack[root_idx] {
+                        ElementType::Binary('&') => return Some(('&', root_idx)),
+                        ElementType::Binary('|') => return Some(('|', root_idx)),
+                        ElementType::Binary('>') => return Some(('>', root_idx)),
+                        ElementType::Binary('-') => return Some(('-', root_idx)),
+                        _ => {}
+                    }
+                }
+
+                return None;
+            }
         }
-
         None
     }
 
