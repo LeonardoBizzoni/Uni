663d3e1d1cb1383fb18a04ad89ccab2c2e3819a2
LeonardoBizzoni <leo2002714@gmail.com>
2023-05-09 15:03:13 +0200

    [Algoritmi] update,min,max,prev,next

--- PrimoAnno/AlgoritmiStruttureDati/linked_list/main.cpp
+++ PrimoAnno/AlgoritmiStruttureDati/linked_list/main.cpp
@@ -2,9 +2,9 @@
 
 int main() {
   node<uint16_t> *head;
-  auto *one = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
-  auto *two = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
-  auto *three = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
+  auto one = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
+  auto two = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
+  auto three = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
 
   one->data = 10;
   two->data = 20;
@@ -18,30 +18,77 @@ int main() {
 
   head = one;
 
-  print_list(head);
+  print_list("Created: ", head);
 
-  auto *new_node = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
+  update(two, (uint16_t)34);
+  print_list("Updated: ",head);
+
+  auto new_node = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
   new_node->data = 56;
   insert(&head, new_node);
-  print_list(head);
+  print_list("Inserted: ", head);
 
-  auto *new_node2 = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
+  auto new_node2 = (node<uint16_t> *)malloc(sizeof(node<uint16_t>));
   new_node2->data = 0;
   insert(&head, new_node2);
-  print_list(head);
+  print_list("Inserted: ", head);
 
-  auto *res_search = search(head, (uint16_t)20);
-  print_node(res_search);
+  auto res_search = search(head, (uint16_t)10);
+  print_node("Search: ", res_search);
+
+  auto min_ptr = min(head);
+  print_node("Min: ", min_ptr);
+
+  auto max_ptr = max(head);
+  print_node("Max: ", max_ptr);
+
+  auto next_ptr = next(head, (uint16_t)30);
+  print_node("Next: ", next_ptr);
+
+  auto prev_ptr = prev(head, (uint16_t)10);
+  print_node("Prev: ", prev_ptr);
 
   remove(&head, new_node);
-  print_list(head);
+  print_list("Removed: ", head);
 
   remove(&head, new_node2);
-  print_list(head);
+  print_list("Removed: ", head);
 
   free(head);
   free(two);
   free(three);
+}
+
+template <typename T> void print_list(node<T> *node) {
+  print_list("", node);
+}
+
+template <typename T> void print_list(const char *prefix, node<T> *node) {
+  if (node == nullptr) {
+    std::cout << std::endl;
+    return;
+  }
+
+  std::cout << prefix << node->data << "\t";
+  print_list(node->next);
+}
+
+template <typename T> void print_node(const char *prefix, node<T> *node) {
+  std::cout << prefix;
+  if (node == nullptr) {
+    std::cout << "NULL" << std::endl;
+    return;
+  }
+
+  std::cout << node << ": ";
+  if (node->prev != nullptr) {
+    std::cout << node->prev->data << " <-";
+  }
+  std::cout << node->data;
+  if (node->next != nullptr) {
+    std::cout << "-> "  << node->next->data;
+  }
+  std::cout << std::endl;
 }
 
 template <typename T> void insert(node<T> **head, node<T> *new_node) {
@@ -78,29 +125,80 @@ template <typename T> node<T> *search(node<T> *head, T key) {
   return current;
 }
 
-template <typename T> void print_list(node<T> *node) {
-  if (node == nullptr) {
-    std::cout << std::endl;
-    return;
-  }
-
-  std::cout << node->data << "\t";
-  print_list(node->next);
+template <typename T> void update(node<T> *node, T key) {
+  node->data = key;
 }
 
-template <typename T> void print_node(node<T> *node) {
-  if (node == nullptr) {
-    std::cout << "NULL" << std::endl;
-    return;
+template <typename T> node<T> *min(node<T> *head) {
+  auto current = head;
+  auto min = current;
+
+  while (current != nullptr) {
+    if (min->data > current->data) {
+      min = current;
+    }
+
+    current = current->next;
   }
 
-  std::cout << node << ": ";
-  if (node->prev != nullptr) {
-    std::cout << node->prev->data << " <-";
+  return min;
+}
+
+template <typename T> node<T> *max(node<T> *head) {
+  auto current = head;
+  auto max = current;
+
+  while (current != nullptr) {
+    if (max->data < current->data) {
+      max = current;
+    }
+
+    current = current->next;
   }
-  std::cout << node->data;
-  if (node->next != nullptr) {
-    std::cout << "-> "  << node->next->data;
+
+  return max;
+}
+
+template <typename T> node<T> *next(node<T> *head, T key) {
+  auto current = head;
+  node<T> *next = nullptr;
+
+  while (next == nullptr && current != nullptr) {
+    if (current->data > key) {
+      next = current;
+    }
+    current = current->next;
   }
-  std::cout << std::endl;
+
+  while (current != nullptr) {
+    if (next->data > current->data && current->data > key) {
+      next = current;
+    }
+
+    current = current->next;
+  }
+
+  return next;
+}
+
+template <typename T> node<T> *prev(node<T> *head, T key) {
+  auto current = head;
+  node<T> *prev = nullptr;
+
+  while (prev == nullptr && current != nullptr) {
+    if (current->data < key) {
+      prev = current;
+    }
+    current = current->next;
+  }
+
+  while (current != nullptr) {
+    if (prev->data < current->data && current->data < key) {
+      prev = current;
+    }
+
+    current = current->next;
+  }
+
+  return prev;
 }
--- PrimoAnno/AlgoritmiStruttureDati/linked_list/main.h
+++ PrimoAnno/AlgoritmiStruttureDati/linked_list/main.h
@@ -12,5 +12,12 @@ template <typename T> struct node {
 template <typename T> void insert(node<T> **head, node<T> *new_node);
 template <typename T> void remove(node<T> **head, node<T> *target_to_remove);
 template <typename T> node<T> *search(node<T> *head, T key);
+template <typename T> void update(node<T> *head, T key);
+template <typename T> node<T> *min(node<T> *head);
+template <typename T> node<T> *max(node<T> *head);
+template <typename T> node<T> *prev(node<T> *head, T key);
+template <typename T> node<T> *next(node<T> *head, T key);
+
 template <typename T> void print_list(node<T> *node);
-template <typename T> void print_node(node<T> *node);
+template <typename T> void print_list(const char *prefix, node<T> *node);
+template <typename T> void print_node(const char *prefix, node<T> *node);
