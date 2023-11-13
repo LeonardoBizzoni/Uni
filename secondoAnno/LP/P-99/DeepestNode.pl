6a71468486d5755b692cb1757659fc3eea8157ce
LeonardoBizzoni <leo2002714@gmail.com>
2023-11-13 20:20:20 +0100

    Avevo sfanculato qualcosa

--- secondoAnno/LP/P-99/DeepestNode.pl
+++ secondoAnno/LP/P-99/DeepestNode.pl
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-deepest_node(node(Key, Value, nil, nil), node(Key, Value, nil, nil), 0).
-
-deepest_node(node(_, _, Left, Right), DN, D) :-
-    Left \= nil,
-    Right \= nil,
-=======
-=======
->>>>>>> Stashed changes
-% deepeset_node(Root, DeepesetNode, Depth)
-
 deepest_node(node(Key, Value, nil, nil),
 	     node(Key, Value, nil, nil),
 	     0).
@@ -23,10 +11,6 @@ deepest_node(node(_, _, nil, Right), DN, D) :-
     D is DRight + 1.
 
 deepest_node(node(_, _, Left, Right), DN, D) :-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     deepest_node(Left, DNLeft, DLeft),
     deepest_node(Right, DNRight, DRight),
     (
@@ -38,19 +22,3 @@ deepest_node(node(_, _, Left, Right), DN, D) :-
 	D is DRight + 1,
 	DN = DNRight
     ).
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-deepest_node(node(_, _, Left, nil), DN, D) :-
-    Left \= nil,
-    deepest_node(Left, DN, DLeft),
-    D is DLeft + 1.
-
-deepest_node(node(_, _, nil, Right), DN, D) :-
-    Right \= nil,
-    deepest_node(Right, DN, DRight),
-    D is DRight + 1.
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
--- secondoAnno/LP/P-99/penultimoElementoInList.pl
+++ secondoAnno/LP/P-99/penultimoElementoInList.pl
@@ -1,13 +1,4 @@
 penultimo([X, _], X).
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 penultimo([_, X | T], Res) :-
-    penultimo([X | T], Res).
-=======
-penultimo([_ | T], Res) :-
-    penultimo(T, Res).
->>>>>>> Stashed changes
-=======
-penultimo([_ | T], Res) :-
-    penultimo(T, Res).
->>>>>>> Stashed changes
+    penultimo([X | T], Res), !.
